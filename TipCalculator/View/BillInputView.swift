983784681ef95e56bad3a2437c587683d909faa5
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-01-14 23:25:22 +0900

    create high level view components with layout

---
+++ TipCalculator/View/BillInputView.swift
@@ -0,0 +1,23 @@
+//
+//  BillInputView.swift
+//  TipCalculator
+//
+//  Created by joe on 1/14/24.
+//
+
+import UIKit
+
+class BillInputView: UIView {
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .green
+    }
+}
---
+++ TipCalculator/View/ResultView.swift
@@ -0,0 +1,23 @@
+//
+//  ResultView.swift
+//  TipCalculator
+//
+//  Created by joe on 1/14/24.
+//
+
+import UIKit
+
+class ResultView: UIView {
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .systemGray5
+    }
+}
