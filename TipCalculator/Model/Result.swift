f05fbcd71fb7720dee7d0d3c86addebe5dd70f10
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-01-23 23:58:39 +0900

    add view model using Combine

---
+++ TipCalculator/Model/Result.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  TipCalculator
+//
+//  Created by joe on 1/23/24.
+//
+
+import Foundation
+
+struct Result {
+    let amountPerPerson: Double
+    let totalBill: Double
+    let totalTip: Double
+}
