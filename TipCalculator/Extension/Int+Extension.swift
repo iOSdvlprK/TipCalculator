75b39a6076ee2aa46d94d56d18d4945756857cce
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-01-28 23:38:16 +0900

    observe split input view

---
+++ TipCalculator/Extension/Int+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Int+Extension.swift
+//  TipCalculator
+//
+//  Created by joe on 1/28/24.
+//
+
+import Foundation
+
+extension Int {
+    var stringValue: String {
+        return String(self)
+    }
+}
