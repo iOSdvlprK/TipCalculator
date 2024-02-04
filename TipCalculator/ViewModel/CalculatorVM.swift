be9d526d7243c141090bced48f12769f6a20cb88
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-02-04 23:59:10 +0900

    implement calculator reset

--- TipCalculator/ViewModel/CalculatorVM.swift
+++ TipCalculator/ViewModel/CalculatorVM.swift
@@ -18,7 +18,7 @@ class CalculatorVM {
     
     struct Output {
         let updateViewPublisher: AnyPublisher<Result, Never>
-        let resultCalculatorPublisher: AnyPublisher<Void, Never>
+        let resetCalculatorPublisher: AnyPublisher<Void, Never>
     }
     
     private var cancellables = Set<AnyCancellable>()
@@ -50,7 +50,7 @@ class CalculatorVM {
         }.eraseToAnyPublisher()
 
         return Output(updateViewPublisher: updateViewPublisher,
-                      resultCalculatorPublisher: resultCalculatorPublisher)
+                      resetCalculatorPublisher: resultCalculatorPublisher)
     }
     
     private func getTipAmount(bill: Double, tip: Tip) -> Double {
